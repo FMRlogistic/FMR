72101bd68c7c47bb93750fed654ee5b50c99d355
Marco <Marco@>
2018-04-10 17:49:12 +0200

    Prova

---
+++ FMR1/src/main/java/view/Home.java
@@ -0,0 +1,36 @@
+package view;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Home extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        resp.setContentType("text/html");
+		
+		req.setAttribute("","");
+		
+		String nextJSP = "/Home.jsp";
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+		dispatcher.forward(req,resp);
+		
+	
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+	
+	
+
+}
